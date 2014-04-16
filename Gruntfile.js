e45fbcbec69f9212ccc4d14369436a8718a4f4a0
Felix Nagel <info@felixnagel.com>
2014-04-16 11:51:07 +0200

    add grunt-cli and fix watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -88,7 +88,7 @@ module.exports = function (grunt) {
 		watch : {
 			scripts: {
 				files : ["<%= jshint.files %>"],
-				tasks : ["jshint"]
+				tasks : ["jshint", "concat"]
 			},
 			styles: {
 				files : ["<%= csslint.files %>"],
